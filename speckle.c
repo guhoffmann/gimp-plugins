83a86f37e9f690e9cbe08ead312e131037d37691
uwe <gert.uwe.hoffmann@gmail.com>
2019-02-06 17:06:58 +0100

    Changed some things...

--- speckle.c
+++ speckle.c
@@ -1,3 +1,8 @@
+/******************************************************************************
+ 'Speckle' with the current brush and color on the current drawable.
+  Size and color can be varied by pseudo random.
+******************************************************************************/
+
 #include <libgimp/gimp.h>
 
 /* Declaration of the mandatory plugin functions */
@@ -9,7 +14,7 @@ static void run (	const gchar *name, gint nparams,	const GimpParam *param,	gint 
 
 GimpPlugInInfo PLUG_IN_INFO = {
 	NULL,		//init, optional - called each time gimp starts |
-	NULL,		//quit, optional - called before closing gimp   |> both are very rarely used
+	NULL,		//quit, optional - called before closing gimp   |> very rarely used
 	query,	// mandatory pointer to query function
 	run		// mandatory pointer to run function
 };
@@ -25,30 +30,19 @@ MAIN()
 static void query (void) {
 
 	static GimpParamDef args[] = {
-		{
-		  GIMP_PDB_INT32,
-		  "run-mode",
-		  "Run mode"
-		},
-		{
-		  GIMP_PDB_IMAGE,
-		  "image",
-		  "Input image"
-		},
-		{
-		  GIMP_PDB_DRAWABLE,
-		  "drawable",
-		  "Input drawable"
-		}
+		{ GIMP_PDB_INT32, "run-mode", "Run mode"},
+		{ GIMP_PDB_IMAGE, "image", "Input image"},
+		{ GIMP_PDB_DRAWABLE, "drawable", "Input drawable"},
+		{ GIMP_PDB_COLOR, "fgcolor",  "Color to draw with"}
 	}; // of GimpParamDerf args[] = ...
 
 	gimp_install_procedure (
 		"plug-in-speckle",									// func. name
 		"Speckle!",									// window title
-		"Displays \"Hello, world!\" in a dialog",	// plugin description
-		"David Neary",										// author
-		"Copyright David Neary",						// copyright notice
-		"2004",												// date created
+		"Speckle drawable with given brush and color",	// plugin description
+		"Uwe Hoffmann",										// author
+		"Copyright Uwe Hoffmann",						// copyright notice
+		"2019",												// date created
 		"_Speckle",								// menu entry name
 		"RGB*, GRAY*",										// image types to work with
 		GIMP_PLUGIN,										// declare this program as external plugin(NOT in core)
@@ -68,8 +62,8 @@ static void run (	const gchar *name, gint nparams,	const GimpParam *param,	gint 
 	GimpRunMode       run_mode;
 	GimpDrawable *drawable;
 	GimpRGB itemRGB;
-	gint32 imageHeight;
-	gint32 imageWidth;
+	gint32 drawableHeight;
+	gint32 drawableWidth;
 	gint32 imageID;
 	gint32 drawableID;
 	gint32 starX;
@@ -89,28 +83,23 @@ static void run (	const gchar *name, gint nparams,	const GimpParam *param,	gint 
 	* we are in NONINTERACTIVE mode */
 	run_mode = param[0].data.d_int32;
 	imageID  = param[1].data.d_int32;
-	
-	imageWidth = gimp_drawable_width(imageID);
-	imageHeight = gimp_drawable_height(imageID);
+	drawableID = param[2].data.d_int32;
+
+	drawableWidth = gimp_drawable_width(drawableID);
+	drawableHeight = gimp_drawable_height(drawableID);
 
 	gimp_context_push();
 	gimp_image_undo_group_start(imageID);
 
-/*	drawableID = gimp_layer_new(imageID,"StarLayer",imageWidth,imageHeight,GIMP_RGB_IMAGE,100,0);
-	gimp_image_insert_layer(imageID, drawableID, 0, 0);*/
-	drawableID = param[2].data.d_int32;
-
-	gimp_context_set_brush("2. Hardness 100");
-
-	for (i = 0; i<1111;i++) {
-		starX = rand() % imageWidth;
-		starY = rand() % imageHeight;
+	for (i = 0; i<300;i++) {
+		starX = rand() % drawableWidth;
+		starY = rand() % drawableHeight;
 		starCol = 96 + rand() % 160;
 		gimp_rgb_set(&itemRGB, starCol/255.0, starCol/255.0, starCol/255.0);
 		points[0] = starX;
 		points[1] = starY;
 		gimp_context_set_foreground(&itemRGB);
-		gimp_context_set_brush_size(1 + rand() % 4);
+		gimp_context_set_brush_size(10 + rand() % 44);
 		gimp_paintbrush_default(drawableID, 2, points);
 	}
 
@@ -125,22 +114,5 @@ static void run (	const gchar *name, gint nparams,	const GimpParam *param,	gint 
 		gimp_message("Hello, world3!\n");
 	}*/
 
-
-/*  Get the specified drawable  */
-	//drawable = gimp_drawable_get (param[2].data.d_drawable);
-	//gimp_progress_init ("My stars...");
-
-	/* Let's time blur
-	*   GTimer timer = g_timer_new time ();
-	*/
-
-	//blur (drawable);
-
-	/*   g_print ("blur() took %g seconds.\n", g_timer_elapsed (timer));
-	*   g_timer_destroy (timer);
-	*/
-
-	//gimp_displays_flush ();
-	//gimp_drawable_detach (drawable);
 } // of static void run ...
 
