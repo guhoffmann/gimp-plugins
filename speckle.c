a098b32e6feed6684c8d3fde7c7a0bd959a3c235
uwe <gert.uwe.hoffmann@gmail.com>
2019-02-07 10:10:34 +0100

    Added parameter saving/initialisation to speckle.c

--- speckle.c
+++ speckle.c
@@ -1,16 +1,33 @@
-/******************************************************************************
- 'Speckle' with the current brush and color on the current drawable.
-  Size and color can be varied by pseudo random.
-******************************************************************************/
+/** speckle.c *****************************************************************
+ *
+ * 'Speckle' with the current brush and color on the current drawable.
+ *  Size and color can be varied pseudo-randomly.
+ *
+ *****************************************************************************/
 
 #include <libgimp/gimp.h>
 
-/* Declaration of the mandatory plugin functions */
+// Define the parameters to be saved for later reuse of plugin
+
+static struct {
+
+  guint numSpeckles, sizeVar, colVar, seed;
+
+} speckleParams  = {
+
+  .numSpeckles = 300,
+  .sizeVar = 10,
+  .colVar = 128,
+  .seed = 1111
+
+};
+
+// Declaration of the mandatory plugin functions
 
 static void query (void);
 static void run (	const gchar *name, gint nparams,	const GimpParam *param,	gint *nreturn_vals, GimpParam **return_vals);
 
-/* This structure declaration is also mandatory */
+// This structure declaration is also mandatory
 
 GimpPlugInInfo PLUG_IN_INFO = {
 	NULL,		//init, optional - called each time gimp starts |
@@ -24,7 +41,7 @@ GimpPlugInInfo PLUG_IN_INFO = {
 MAIN()
 
 /*******************************************************************************
- * query: registration and input arguments definition.
+ ** query: registration and input arguments definition.
  */
 
 static void query (void) {
@@ -57,6 +74,9 @@ static void query (void) {
 
 } //static void query(void)...
 
+/*******************************************************************************
+ ** run: main actions of the plugin.
+ */
 
 static void run (	const gchar *name, gint nparams,	const GimpParam *param,	gint *nreturn_vals, GimpParam **return_vals) {
 
@@ -65,52 +85,78 @@ static void run (	const gchar *name, gint nparams,	const GimpParam *param,	gint 
 	GimpRunMode		run_mode;
 	GimpDrawable	*drawable;
 	GimpRGB			itemRGB, startRGB, workRGB;
-	guint32			drawableHeight,  drawableWidth,
-						imageID, drawableID,
-						starX, starY, seed, numSpeckles, sizeVar;
-	gfloat starCol;
+	guint				drawableHeight,  drawableWidth, imageID, drawableID,
+						speckleX, speckleY ;
+	gfloat speckleCol;
 	int i;
 	gdouble points[2];
 	gdouble brushSize;
 
-	/* Setting mandatory output values */
+	// Setting mandatory output values
 	*nreturn_vals = 1;
 	*return_vals  = values;
 
 	values[0].type = GIMP_PDB_STATUS;
 	values[0].data.d_status = status;
 
-	/* Getting run_mode - we won't display a dialog if
-	* we are in NONINTERACTIVE mode */
-	run_mode = param[0].data.d_int32;
-	imageID  = param[1].data.d_int32;
-	drawableID = param[2].data.d_int32;
-	numSpeckles = param[3].data.d_int32;
-	seed = param[4].data.d_int32;
+	run_mode		= param[0].data.d_int32;
+	imageID		= param[1].data.d_int32;
+	drawableID	= param[2].data.d_int32;
 
-	drawableWidth = gimp_drawable_width(drawableID);
+	drawableWidth	= gimp_drawable_width(drawableID);
 	drawableHeight = gimp_drawable_height(drawableID);
 
-	/* Begin the main actions context */
+	// Begin the main actions context
 
 	gimp_context_push();
 	gimp_image_undo_group_start(imageID);
-	
-	srand(seed);
+
+	switch (run_mode) {
+
+		case GIMP_RUN_INTERACTIVE:
+			// Get options last values if needed
+			gimp_get_data ("plug-in-speckle", &speckleParams);
+
+			// Display the dialog
+			//if (! blur_dialog (drawable))
+		  	//return;
+			break;
+
+		case GIMP_RUN_NONINTERACTIVE:
+			if (nparams != 7)
+			  status = GIMP_PDB_CALLING_ERROR;
+			if (status == GIMP_PDB_SUCCESS)
+		  		speckleParams.numSpeckles	= param[3].data.d_int32;
+				speckleParams.sizeVar		= param[4].data.d_int32;
+				speckleParams.colVar			= param[5].data.d_int32;
+				speckleParams.seed			= param[6].data.d_int32;
+			break;
+
+		case GIMP_RUN_WITH_LAST_VALS:
+			//  Get options last values if needed
+			gimp_get_data ("plug-in-speckle", &speckleParams);
+			break;
+
+		default:	break;
+	}	
+
+	srand(speckleParams.seed);
 	gimp_context_get_foreground(&startRGB);
 	brushSize = gimp_context_get_brush_size();
+	
+	//numSpeckles = 300;
 
-	for (i = 0; i<500;i++) {
-		starX = rand() % drawableWidth;
-		starY = rand() % drawableHeight;
-		starCol = rand() % 128/128.0;
+	for (i = 0; i < speckleParams.numSpeckles; i++) {
+		speckleX = rand() % drawableWidth;
+		speckleY = rand() % drawableHeight;
+		speckleCol = rand() % speckleParams.colVar/(speckleParams.colVar*1.0);
 		itemRGB = startRGB;
-		gimp_rgb_set(&workRGB, starCol, starCol, starCol);
+		gimp_rgb_set(&workRGB, speckleCol, speckleCol, speckleCol);
 		gimp_rgb_subtract(&itemRGB, &workRGB);
-		points[0] = starX;
-		points[1] = starY;
+		points[0] = speckleX;
+		points[1] = speckleY;
 		gimp_context_set_foreground(&itemRGB);
-		gimp_context_set_brush_size(brushSize + rand() % 44);
+		gimp_context_set_brush_size(brushSize + rand() % speckleParams.sizeVar);
 		gimp_paintbrush_default(drawableID, 2, points);
 	}
 
@@ -118,15 +164,13 @@ static void run (	const gchar *name, gint nparams,	const GimpParam *param,	gint 
 	
 	gimp_context_pop();
 	gimp_displays_flush ();
+	//	gimp_drawable_detach (drawableID);
 
 	/* End of main actions context */
 
-//	gimp_drawable_detach (drawableID);
-
-/*	if (run_mode != GIMP_RUN_NONINTERACTIVE) {
-		gimp_message_set_handler(GIMP_MESSAGE_BOX);
-		gimp_message("Hello, world3!\n");
-	}*/
+	//  Finally, set options in the core
+	if (run_mode == GIMP_RUN_INTERACTIVE)
+		gimp_set_data ("plug-in-speckle", &speckleParams, sizeof (speckleParams));
 
 } // of static void run ...
 
